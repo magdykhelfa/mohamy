22bf37bae99cecfbe885c35f4a7cbbbc7eb5e64e
magdykhelfa <magdy.khelfa.eg@gmail.com>
2026-01-05 17:01:53 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,19 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/mohamy/", 
+  // ⚠️ لازم اسم الريبو حرفيًا
+  base: mode === "production" ? "/mohamy/" : "/",
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [
     react(),
     mode === "development" && componentTagger(),
   ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
