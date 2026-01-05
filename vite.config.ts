aee16ba8c6cd75e0f5b4194ecca52c5247ade52e
magdykhelfa <magdy.khelfa.eg@gmail.com>
2026-01-05 16:29:49 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,16 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./", // <--- السطر ده مهم جداً عشان الـ CSS والـ JS يفتحوا صح
+  // حطينا اسم الريبو بتاعك هنا عشان الصور والملفات تفتح صح
+  base: "/mohamy/", 
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
