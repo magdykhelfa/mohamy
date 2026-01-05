973b3bc2cd01c9e457e23becf28939adba480577
magdykhelfa <magdy.khelfa.eg@gmail.com>
2026-01-05 16:19:43 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // <--- السطر ده مهم جداً عشان الـ CSS والـ JS يفتحوا صح
   server: {
     host: "::",
     port: 8080,
